501724ec3d24ec8a9bb33d9bc6c42939e6938ecb
RockW <rock7699@gmail.com>
2020-05-29 16:00:45 -0700

    adds store model

--- models/Store.js
+++ models/Store.js
@@ -1,1 +1,10 @@
- 
+const mongoose = require("mongoose");
+
+const storeSchema = new mongoose.Schema({
+    name: {type: String, required: true},
+    type: {type:String, required: true},
+    expensive: {type: String, required: true}
+});
+
+const Store = mongoose.model("Store", storeSchema);
+module.exports = Store;
