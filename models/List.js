3ad4db94c873f11b4774afdcf04a96c312586fde
RockW <rock7699@gmail.com>
2020-05-30 16:55:42 -0700

    adds list model and includes references to product and store

---
+++ models/List.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const listSchema = new mongoose.Schema({
+    name: {type: String, required: true},
+    store: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Store'
+    }],
+    products: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Product'
+    }]
+});
+
+const List = mongoose.model("List", ListSchema);
+module.exports = List;
