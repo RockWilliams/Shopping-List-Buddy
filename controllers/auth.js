30b66172154131f0aa328cca81f0b36aead6d9fc
Pavaloz99 <avalozp@gmail.com>
2020-06-01 09:33:14 -0700

    adds functionality to register form and hashes password

--- controllers/auth.js
+++ controllers/auth.js
@@ -8,5 +8,22 @@ router.get("/register", function(req, res){
     res.render("User/register");
 });
 
+router.post('/register', async function(req, res){
+    try {
+        const foundUser = await db.User.findOneAndDelete({email: req.body.email});
+        if(foundUser) {
+            return res.send({message: "Account is already registered"});
+        }
+        const salt = await bcrypt.genSalt(10);
+        const hash = await bcrypt.hash(req.body.password, salt);
+        req.body.password = hash;
+        const newUser = await db.User.create(req.body);
+        console.log(newUser);
+        res.redirect('/login');
+    } catch (err) {
+        res.send({message: 'Internal Server Error', error: err});
+      }
+    });   
+
 module.exports = router;
 
