570661b92e53f698d06b4a29a600441d3a9f5a46
Pavaloz99 <avalozp@gmail.com>
2020-06-01 16:45:01 -0700

    appends list entries onto new list page

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,4 +1,29 @@
 console.log("hello there");
-$('p').on('click', function(){
-    console.log('hello');
-})
+$('option').on('click', function(){
+    console.log("help")
+});
+
+
+
+//source for the following code http://jsfiddle.net/UziTech/cjjg68dr/114/
+$("option").mousedown(function(element){
+    element.preventDefault();
+
+    let select = this;
+    let scroll = select.scrollTop;
+
+    element.target.selected = !element.target.selected;
+
+    setTimeout(function(){select.scrollTop = scroll;}, 0);
+
+    $(select).focus();
+}).mousemove(function(element){element.preventDefault()});
+
+$("option").on('click', function(){
+    $('#list').append($('<li></li>').text(this.text));
+    console.log($('#list').length);
+});
+
+for(let i = 0; i < $('#list').length; i++){
+
+}
