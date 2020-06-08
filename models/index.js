9279e79d82893e2f99b154a2836d0155d1eb7de4
RockW <rock7699@gmail.com>
2020-06-08 14:37:34 -0700

    adds heroku deployment config

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const connectionString = "mongodb://localhost:27017/store";
+const connectionString = process.env.MONGODB_URI || "mongodb://localhost:27017/store";
 
 mongoose.connect(connectionString, {
     useNewUrlParser: true,
